19d44cc78122cd5ac663182488b38cd040287112
FujiHaruka <fizzyjohn1427@gmail.com>
2021-11-05 13:24:06 +0900

    improve messages

--- cli.ts
+++ cli.ts
@@ -1,11 +1,14 @@
-import { assertEquals, BufReader } from "./deps.ts";
+import { assertEquals, BufReader, Color } from "./deps.ts";
 
-console.log("Text A");
+console.log(
+  "You can enter multiple-line text and finish entering text by ctrl+D on the beginning of a line",
+);
+console.log(Color.bold("Enter the first text (actual):"));
 
 const readerA = new BufReader(Deno.stdin);
 const inputA = await readerA.readString(String.fromCharCode(26));
 
-console.log("Text C");
+console.log(Color.bold("Enter the second text (expected):"));
 
 const readerB = new BufReader(Deno.stdin);
 const inputB = await readerB.readString(String.fromCharCode(26));
@@ -19,7 +22,11 @@ main(inputA, inputB);
 function main(str1: string, str2: string) {
   try {
     assertEquals(str1, str2);
+    console.log("");
+    console.log("");
+    console.log(Color.bold("The given two texts are equal."));
   } catch (err) {
+    console.log("");
     console.log(err.message);
   }
 }
--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
-export { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
-export { BufReader } from "https://deno.land/std@0.106.0/io/bufio.ts";
+export * as Color from "https://deno.land/std@0.113.0/fmt/colors.ts";
+export { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts";
+export { BufReader } from "https://deno.land/std@0.113.0/io/bufio.ts";
