5db32360b273ceacf8f7f8ca7dcf72f51399945f
FujiHaruka <fizzyjohn1427@gmail.com>
2022-03-27 19:34:54 +0900

    update deps

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export * as Color from "https://deno.land/std@0.116.0/fmt/colors.ts";
-export { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
+export * as Color from "https://deno.land/std@0.132.0/fmt/colors.ts";
+export { assertEquals } from "https://deno.land/std@0.132.0/testing/asserts.ts";
 export {
   BufReader,
   readStringDelim,
-} from "https://deno.land/std@0.116.0/io/buffer.ts";
+} from "https://deno.land/std@0.132.0/io/buffer.ts";
